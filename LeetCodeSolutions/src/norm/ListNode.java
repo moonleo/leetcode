c38fe967ac8fe7fa8a29159fb2a9a81dbed45e99
moonleo <yanhan_90@126.com>
2015-08-26 20:13:49 +0800

    为链表类添加一些常用方法

--- LeetCodeSolutions/src/norm/ListNode.java
+++ LeetCodeSolutions/src/norm/ListNode.java
@@ -12,4 +12,26 @@ public class ListNode {
 	public ListNode(int x) {
 		val = x;
 	}
+	
+	public static ListNode array2List(int[] array) {
+		ListNode head = new ListNode(array[0]);
+		ListNode temp = head;
+		for(int i=1; i<array.length; i++) {
+			ListNode current = new ListNode(array[i]);
+			temp.next = current;
+			temp = current;
+		}
+		return head;
+	}
+	
+	public static void display(ListNode head) {
+		ListNode temp = head;
+		if(null == temp) {
+			System.out.println("null");
+		}
+		while(null != temp) {
+			System.out.print(temp.val+" ");
+			temp = temp.next;
+		}
+	}
 }
