d44560f8275b8a7aeb20d4072815a2c5cb1f5ba0
moonleo <yanhan_90@126.com>
2015-10-07 21:32:54 +0800

    数组工具类

---
+++ LeetCodeSolutions/src/norm/ArraysUtil.java
@@ -0,0 +1,18 @@
+package norm;
+
+public class ArraysUtil {
+	public static int[] create(int... args) {
+		int[] result = new int[args.length];
+		for(int i=0; i<args.length; i++) {
+			result[i] = args[i];
+		}
+		return result;
+	}
+	
+	public static void print(int[] array) {
+		for(int i: array) {
+			System.out.print(i+" ");
+		}
+		System.out.println();
+	}
+}
