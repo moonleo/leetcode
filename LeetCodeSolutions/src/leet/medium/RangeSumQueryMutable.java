23a3415188ac6f36eb9dc3f9b818f3d853125188
moonleo <yanhan_90@126.com>
2015-11-25 16:03:31 +0800

    modified index

--- LeetCodeSolutions/src/leet/medium/RangeSumQueryMutable.java
+++ LeetCodeSolutions/src/leet/medium/RangeSumQueryMutable.java
@@ -1,7 +1,7 @@
 package leet.medium;
 
 /**
- * No.372 Range Sum Query-Mutable
+ * No.307 Range Sum Query-Mutable
  * 
  * Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive. 
  * The update(i, val) function modifies nums by updating the element at index i to val.
